5e2520fd13092e89ec6b6d75c019033d0732f84d
filipa.ferreira <ferreiraa.ffilipa@gmail.com>
2025-11-02 10:55:31 +0000

    update

--- src/pt/ulusofona/lp2/greatprogrammingjourney/Slot.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/Slot.java
@@ -1,28 +1,27 @@
 package pt.ulusofona.lp2.greatprogrammingjourney;
 
 public class Slot {
-    int number;
-    Player ocupant;
+    /* fields */
+    private int index;
 
-
-    public Slot(int number){
-        this.number = number;
-        this.ocupant = null;
-    }
-
-    public boolean isOccupied (){
-        return ocupant != null;
-    }
-
-    public Player getOcupant(){
-        return ocupant;
-    }
-
-    public void setOcupant(Player player){
-        this.ocupant = player;
-    }
-
-    public int getNumber(){
-        return number;
-    }
+//    public Slot(int number){
+//        this.number = number;
+//        this.ocupant = null;
+//    }
+//
+//    public boolean isOccupied (){
+//        return ocupant != null;
+//    }
+//
+//    public Player getOcupant(){
+//        return ocupant;
+//    }
+//
+//    public void setOcupant(Player player){
+//        this.ocupant = player;
+//    }
+//
+//    public int getNumber(){
+//        return number;
+//    }
 }
